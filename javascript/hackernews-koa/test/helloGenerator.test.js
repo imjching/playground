67432448ccbd2d33f0d0ff57ddbc18a2d93fd160
Jay C <imjching@hotmail.com>
2016-01-03 23:55:52 +0800

    generator functions, mocha

---
+++ javascript/hackernews-koa/test/helloGenerator.test.js
@@ -0,0 +1,12 @@
+// test the helloGenerator function
+
+require('co-mocha');
+var expect = require('chai').expect;
+var helloGenerator = require('../src/helloGenerator');
+
+describe('Hello Generator', function() {
+  it('should yield to the function and return hello', function *() {
+    var ans = yield helloGenerator();
+    expect(ans).to.equal('hello generator');
+  });
+});
