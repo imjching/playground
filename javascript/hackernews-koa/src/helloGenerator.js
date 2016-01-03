b39f8b5a2c3a72e96bc64e203436267a4fc1e7fb
Jay C <imjching@hotmail.com>
2016-01-04 00:44:16 +0800

    once we use generator functions, we should callbacks with care;

--- javascript/hackernews-koa/src/helloGenerator.js
+++ javascript/hackernews-koa/src/helloGenerator.js
@@ -1,3 +1,29 @@
+var sleep = require('co-sleep');
+
 module.exports = function *() {
+  yield sleep(1000);
   return 'hello generator';
 };
+
+// tests will fail this way, so alternatives:
+// 1. thunkify the function (taking a normal node with a callback pattern so we can yield)
+// 2. co-sleep
+// module.exports = function *() {
+//   setTimeout(function() {
+//     return 'hello generator';
+//   }, 1000);
+// };
+
+// when using it outside koa.js
+// var co = require('co');
+// var sleep = require('co-sleep');
+
+// co(function* (){
+//   console.log('1');
+//   yield sleep(1000);
+//   console.log('3');
+// });
+
+// console.log('2');
+
+// returns 1, 2, 3
