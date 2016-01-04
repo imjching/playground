bd0565698830e834883b6856ce3ae1c8dbc8988f
Jay C <imjching@hotmail.com>
2016-01-04 16:26:08 +0800

    content negotiation

--- javascript/koajs-workshop/06-content-negotiation/index.js
+++ javascript/koajs-workshop/06-content-negotiation/index.js
@@ -10,10 +10,20 @@ var koa = require('koa');
  *     this.response.body = yield gzip('something');
  *   })
  */
+// Using this.request.acceptsEncodings(), either send hello world gzipped
+// or not gzipped (identity). For the purposes of this test, always set the
+// Content-Encoding header, even if it's just identity.
+
 var gzip = require('mz/zlib').gzip;
 
 var app = module.exports = koa();
 
 app.use(function* () {
-
+  if (!this.acceptsEncodings('gzip')) {
+    this.response.set('Content-Encoding', 'identity');
+    this.body = 'hello world';
+  } else {
+    this.response.set('Content-Encoding', 'gzip');
+    this.body = yield gzip('hello world');
+  }
 })
