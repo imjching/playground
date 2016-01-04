0550216168d54e2f71f2e11aecdb75bb0dd5ee76
Jay C <imjching@hotmail.com>
2016-01-04 16:28:48 +0800

    content headers

--- javascript/koajs-workshop/07-content-headers/index.js
+++ javascript/koajs-workshop/07-content-headers/index.js
@@ -3,6 +3,16 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
+// Create an app that checks the Content-Type of the request.
+// If it's application/json, return {message: 'hi!'} with appropriate content
+// headers. Otherwise, return ok as a string.
+// https://github.com/koajs/koa/blob/master/docs%2Fapi%2Fcontext.md
+
 app.use(function* () {
-  
+  if (this.is('application/json')) {
+    this.type = 'application/json';
+    this.body = { message : 'hi!' };
+  } else {
+    this.body = 'ok';
+  }
 })
