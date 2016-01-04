d481db57767b60889ba87da0854a534df4d0e3c1
Jay C <imjching@hotmail.com>
2016-01-04 15:56:11 +0800

    koa-routing

--- javascript/koajs-workshop/03-routing/index.js
+++ javascript/koajs-workshop/03-routing/index.js
@@ -1,16 +1,30 @@
 
+// this.request.path
+// console.log(this.request.method); // get/post
+// console.log(this.request.query); // query string
+// console.log(this.request.host); // host
+
 var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
-
+app.use(function* (next) {
+  if (this.request.path !== '/') {
+    return yield next;
+  }
+  this.body = 'hello world';
 });
 
-app.use(function* () {
-
+app.use(function* (next) {
+  if (this.request.path !== '/404') {
+    return yield next;
+  }
+  this.body = 'page not found';
 });
 
-app.use(function* () {
-
+app.use(function* (next) {
+  if (this.request.path !== '/500') {
+    return yield next;
+  }
+  this.body = 'internal server error';
 });
