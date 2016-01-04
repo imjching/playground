b6a7bbd0cdc5931c78c955f01bdb8c02ec76e24c
Jay C <imjching@hotmail.com>
2016-01-04 16:35:27 +0800

    rendering jade layouts in koa

--- javascript/koajs-workshop/09-templating/index.js
+++ javascript/koajs-workshop/09-templating/index.js
@@ -5,7 +5,13 @@ var path = require('path');
 
 var app = module.exports = koa();
 
+// Render homepage.jade using Jade's API.
+// Because we use Jade's extends feature, you probably need to use
+// Jade's asynchronous API as well as the .filename option.
+// ref: http://jade-lang.com/api/
+
 app.use(function* () {
   var filename = path.join(__dirname, 'homepage.jade');
-  // this.response.body=
+
+  this.response.body = jade.renderFile(filename);
 });
