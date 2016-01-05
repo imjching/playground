d5cef7c9db5b31a06066c649e9ff39922fa5e423
Jay C <imjching@hotmail.com>
2016-01-05 21:47:25 +0800

    [codewars] Array.reduce

---
+++ javascript/codewars/sum-arrays.js
@@ -0,0 +1,9 @@
+//http://www.codewars.com/kata/sum-arrays
+
+// create a function which sums all the numbers in array
+const sum = (arr) => arr.reduce((e, v) => e + v, 0);
+
+// normal function
+function sum (arr) {
+  return arr.reduce((e, v) => e + v, 0);
+}
