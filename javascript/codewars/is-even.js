aa353eb15497d511d1bd0746e7d3c4b4e600432f
Jay C <imjching@hotmail.com>
2016-01-05 22:31:15 +0800

    [codewars] Array.filter; checking if numbers are even; bitwise operators

---
+++ javascript/codewars/is-even.js
@@ -0,0 +1,15 @@
+// check if a number is even
+// http://www.codewars.com/kata/javascript-array-filter/javascript
+
+// normal solution
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(e => e % 2 == 0);
+
+  // or
+  // return numbersArray.filter(e => !(e % 2));
+
+  // or, with bitwise operators
+  // number is even if the last bit is 0
+  // e & 1 returns 1 if number is odd
+  return numbersArray.filter(e => !(e & 1));
+}
