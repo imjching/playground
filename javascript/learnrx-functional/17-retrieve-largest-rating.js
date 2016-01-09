cc57a7fcf28bed0b1ebe6f1095db82af9dcd9071
Jay C <imjching@hotmail.com>
2016-01-09 22:46:45 +0800

    Reducing Arrays

---
+++ javascript/learnrx-functional/17-retrieve-largest-rating.js
@@ -0,0 +1,16 @@
+// Exercise 17: Retrieve the largest rating.
+
+// Let's use our new reduce function to isolate the largest value in an
+// array of ratings.
+
+function() {
+  var ratings = [2,3,1,4,5];
+
+  // You should return an array containing only the largest rating. Remember that reduce always
+  // returns an array with one item.
+  return ratings.
+    reduce   // Complete this expression
+}
+
+// Nice work. Now let's try combining reduce() with our other functions to
+// build more complex queries.
