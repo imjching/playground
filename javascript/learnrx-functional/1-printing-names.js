4e5fdc4ec8d6c5a9d5d6716358fb481dbf530ca0
Jay C <imjching@hotmail.com>
2016-01-09 22:26:21 +0800

    added learnrx-functional; traversing with arrays and projecting with arrays exercises

---
+++ javascript/learnrx-functional/1-printing-names.js
@@ -0,0 +1,13 @@
+// Exercise 1: Print all the names in an array
+
+function(console) {
+  var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+      counter;
+
+  for (counter = 0; counter < names.length; counter++) {
+    // code
+  }
+}
+
+// Ask yourself this question: did we need to specify the order in which
+// the names were printed? If not, why do it?
---
+++ javascript/learnrx-functional/2-using-forEach-printing-names.js
@@ -0,0 +1,12 @@
+// Exercise 2: Use forEach to print all the names in an array
+
+// Let's repeat the previous exercise using the forEach function.
+
+function(console) {
+  var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+  // code here
+}
+
+// Notice that forEach lets us specify what we want to happen to each item in
+// the array, but hides how the array is traversed.
