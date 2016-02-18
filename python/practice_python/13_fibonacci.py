231e0bf9450c28e02e19d6620f6f60c85946ccc2
Jay <imjching@hotmail.com>
2016-02-18 23:52:00 +0800

    added fibonacci solution

--- python/practice_python/13_fibonacci.py
+++ python/practice_python/13_fibonacci.py
@@ -8,5 +8,18 @@ user to enter the number of numbers in the sequence to
 generate.(Hint: The Fibonnaci seqence is a sequence of
 numbers where the next number in the sequence is the sum
 of the previous two numbers in the sequence. The sequence
-looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+looks like this: 1, 1, 2, 3, 5, 8, 13,...)
 """
+
+count = int(raw_input("How many Fibonacci numbers do you want? "))
+
+# n is the nth term
+def get_fibonacci_number(n):
+  if n < 3:
+    return 1
+  return get_fibonacci_number(n - 1) + get_fibonacci_number(n - 2)
+
+result = ""
+for i in range(count):
+  result += str(get_fibonacci_number(i + 1)) + ", "
+print result[:len(result) - 2]
