9ef89bbed53d86354a270b48fe1727e2aa638551
Jay <imjching@hotmail.com>
2016-02-21 23:55:41 +0800

    work in progress

--- python/practice_python/16_password_generator.py
+++ python/practice_python/16_password_generator.py
@@ -12,3 +12,37 @@ Ask the user how strong they want their password to be. For weak passwords,
 pick a word or two from a list.
 """
 
+from random import randint
+
+def generate_password(length):
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  numbers = "1234567890"
+  symbols = "!@#$%^&*()_+-="
+  everything = alpha + numbers + symbols
+
+  password = ""
+
+  while len(password) != length:
+    password += randint(0, len(everything) - 1)
+
+  if check_password(password): #valid/secure
+    return password
+
+def main():
+  print "Generating password..."
+
+  password_length = -1
+  while True:
+    password_length = int(raw_input("What's your password length? (min 4)"))
+    if password_length < 4:
+      print "Password length must be more than or equals to 4"
+    else:
+      break
+
+
+
+
+
+
+
+main()
