96d7c3c811672a7bca38ded8103380234dcf3046
Jay <imjching@hotmail.com>
2016-02-22 00:01:07 +0800

    complete challenge 16

--- python/practice_python/16_password_generator.py
+++ python/practice_python/16_password_generator.py
@@ -14,35 +14,48 @@ pick a word or two from a list.
 
 from random import randint
 
+def check_password(password):
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  numbers = "1234567890"
+  symbols = "!@$&*"
+  check = [False, False, False, False] # alpha, alpha_upper, numbers, symbols
+  for i in password:
+    if i in alpha:
+      check[0] = True
+    elif i in alpha.upper():
+      check[1] = True
+    elif i in numbers:
+      check[2] = True
+    else:
+      check[3] = True
+  return check[0] == check[1] == check[2] == check[3] == True
+
 def generate_password(length):
   alpha = "abcdefghijklmnopqrstuvwxyz"
   numbers = "1234567890"
-  symbols = "!@#$%^&*()_+-="
-  everything = alpha + numbers + symbols
+  symbols = "!@$&*"
+  everything = alpha + alpha.upper() + numbers + symbols
 
   password = ""
 
   while len(password) != length:
-    password += randint(0, len(everything) - 1)
+    password += everything[randint(0, len(everything) - 1)]
 
   if check_password(password): #valid/secure
     return password
+  return generate_password(length)
 
 def main():
   print "Generating password..."
 
   password_length = -1
   while True:
-    password_length = int(raw_input("What's your password length? (min 4)"))
+    password_length = int(raw_input("What's your password length? (min 4) "))
     if password_length < 4:
       print "Password length must be more than or equals to 4"
     else:
       break
 
-
-
-
-
-
+  print "Your generated password is", generate_password(password_length)
 
 main()
