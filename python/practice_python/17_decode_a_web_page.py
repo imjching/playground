33335d46fb0dc95996b51cd95c524d64481d7971
Jay <imjching@hotmail.com>
2016-02-23 19:26:44 +0800

    add challenge 17: decode a web page

---
+++ python/practice_python/17_decode_a_web_page.py
@@ -0,0 +1,6 @@
+# http://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
+
+"""
+Use the BeautifulSoup and requests Python packages to print out a list of
+all the article titles on the New York Times homepage.
+"""
