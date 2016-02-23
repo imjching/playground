e643e0f3c26e7c6fa0a7db90d8b3b0377b2d9ea0
Jay <imjching@hotmail.com>
2016-02-23 19:40:18 +0800

    complete challenge 17: managed to grab all article titles from NYT

--- python/practice_python/17_decode_a_web_page.py
+++ python/practice_python/17_decode_a_web_page.py
@@ -4,3 +4,18 @@
 Use the BeautifulSoup and requests Python packages to print out a list of
 all the article titles on the New York Times homepage.
 """
+
+from bs4 import BeautifulSoup
+import requests
+
+webpage = requests.get("http://www.nytimes.com/")
+contents = webpage.text
+soup = BeautifulSoup(contents, "html.parser")
+
+all_h2s = soup.find_all("h2", { "class": "story-heading" })
+all_titles = map(lambda x: x.find("a"), all_h2s)
+all_titles = [i for i in all_titles if i is not None]
+all_titles = map(lambda x: x.text, all_titles)
+
+for i in all_titles:
+  print i.strip()
